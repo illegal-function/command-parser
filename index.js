6a1519fa999dffefd1ca237be99d902b1e146533
illegal-function <22005328+illegal-function@users.noreply.github.com>
2019-06-02 13:32:35 +0200

    remove test code from index.js

--- index.js
+++ index.js
@@ -8,25 +8,3 @@ module.exports = (string, specs, env = {}) => {
 
   return validate(parsed, specs, env)
 }
-
-const spec = [
-  { name: 'lib', commands: [
-    { name: 'command', params: [
-      { name: 'arg', type: 'string' },
-      { name: 'a2', type: 'string' },
-    ] },
-  ] },
-  { name: 'test', commands: [
-    { name: 'command2', params: [
-      { name: 'v123', type: 'string' },
-    ] },
-  ] },
-]
-
-console.log(
-  module.exports(
-    '?lib command arg:$test a2: { ?test command2 v123:"dette er ${test} test" }',
-    spec,
-    { test: 'ENV_VARIABLE' },
-  )
-)
